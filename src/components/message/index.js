b67d27b477d5d604c76b747d05bc065fb87ce109
bubizm <72682122+bubizm@users.noreply.github.com>
2022-06-15 19:54:26 +0300

    Update index.js

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -17,7 +17,6 @@ export const Message = ({ msgId, message, author }) => {
   const { chatId } = useParams();
 
   const handleDelete = (id) => {
-    // dispatch(deleteMessage(chatId, id));
     remove(getMessagesRefById(chatId, id));
   };
 
@@ -25,25 +24,9 @@ export const Message = ({ msgId, message, author }) => {
     dispatch(editMessage(chatId, id, 'hi'));
   };
 
-  // const handleAddMessage = (text) => handleEdit(text);
-
-  // const handleEditMsg = (id) => {
-  //   const handleAddMessage = (text) => handleEdit(text, id);
-  //   console.log(handleAddMessage);
-  //   // <Form onSubmit={handleAddMessage} />;
-  //ПОПЫТКИ СДЕЛАТЬ ИЗМЕНЕНИЕ СООБЩЕНИЯ РАЗНЫМ ТЕКСТОМ
-  // const handleEdit = (id, text) => {
-  //   <Form onSubmit={handleAddMessage} />;
-  //   // const handleNewText = <Form onSubmit={handleAddMessage} />;
-  //   dispatch(editMessage(chatId, id, text));
-  // };
-
   return (
     <div className='msg'>
       <button onClick={() => handleEdit(msgId)}>Edit</button>
-      {/* <h3 className='header'>
-        {author}: {message}
-      </h3> */}
       <MessagePresentation author={author} message={message} />
       <button onClick={() => handleDelete(msgId)}>X</button>
     </div>
