abffe44f20a66d1ca14443fbd834cc4f1073e225
bubizm <72682122+bubizm@users.noreply.github.com>
2022-06-15 19:58:21 +0300

    Update index.js

--- src/components/DeleteChat/index.js
+++ src/components/DeleteChat/index.js
@@ -6,25 +6,9 @@ import { deleteChat } from '../../store/chats/actions';
 import './style.css';
 
 export const DeleteButton = (id) => {
-  // const deleteChat = useCallback(() => del(id));
-
-  // const dispatch = useDispatch();
 
   const handleDeleteChat = () => {
-    // dispatch(deleteChat(id));
-
-    // set(getChatsRefById(id.id), null);
     remove(getChatsRefById(id.id));
-
-    // setChatList((prevChatList) =>
-    //   prevChatList.filter((chat) => chat.id !== chatId)
-    // );
-    // setMessages((prevMessages) => {
-    //   const newMessages = { ...prevMessages };
-
-    //   delete newMessages[chatId];
-    //   return newMessages;
-    // });
   };
 
   return <button onClick={handleDeleteChat}>Удалить</button>;
